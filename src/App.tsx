9c587d803b5cd84fc9c9c6354ae61f60612fd0ec
Armando Rios <armandorios.dev@gmail.com>
2025-04-25 22:07:43 -0700

    feat: add basic functionality for search and filter pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,45 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
 import Search from './components/pokemon/Search';
+import { getPokemon } from './ultils/getPokemon';
+import PokemonCard from './components/pokemon/PokemonCard';
+
+interface Pokemon {
+  name: string;
+  id: number;
+  types: string[];
+}
 
 const App = () => {
   const [search, setSearch] = useState('');
+  const [allPokemons, setAllPokemons] = useState<Pokemon[]>([]);
+  const [filters, setFilters] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    getPokemon('/public/pokemon-data.json').then((data: Pokemon[]) => {
+      setAllPokemons(data);
+    });
+  }, []);
+
+  useEffect(() => {
+    setFilters(
+      allPokemons.filter((pokemon: Pokemon) => {
+        return pokemon.name.toLowerCase().includes(search.toLowerCase());
+      })
+    );
+  }, [search]);
 
   return (
     <div className="flex flex-col gap-2 h-screen w-screen items-center">
       <Header />
       <main className="flex-1 flex flex-col items-center gap-4">
         <Search setSearch={setSearch} />
+        <div className="flex gap-4 flex-wrap justify-center">
+          {filters.map((pokemon) => {
+            return <PokemonCard data={pokemon} />;
+          })}
+        </div>
       </main>
       <Footer />
     </div>
